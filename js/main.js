91c0f9b1414bbaba761f0430709fca2237520ae5
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-01-28 05:30:26 +0100

    [dev] Animation de la bulle "Contactez-moi"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+jQuery(function($){
 
+    $('.vcard .say').hide();
+    $('.vcard .email').hover(function(){
+        $('.vcard .say').stop(true, false).fadeIn().animate({opacity:1});
+    },function(){
+        $('.vcard .say').fadeOut();
+    });
+
+});
