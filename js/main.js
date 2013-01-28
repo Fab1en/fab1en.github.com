05811f146bf929ae61e0347628b90c988c24f77a
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-01-28 16:05:52 +0100

    [dev] Animer la vCard

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 jQuery(function($){
 
-    $('.vcard .say').hide();
-    $('.vcard .email').hover(function(){
-        $('.vcard .say').stop(true, false).fadeIn().animate({opacity:1});
-    },function(){
-        $('.vcard .say').fadeOut();
-    });
+    function showContactMe(){
+        $('.vcard .say').hide();
+        $('.vcard .email').hover(function(){
+            $('.vcard .say').stop(true, false).fadeIn().animate({opacity:1});
+        },function(){
+            $('.vcard .say').fadeOut();
+        });
+    }
+    
+    function setupHeader(){
+        if($(window).scrollTop() == 0) $('.vcard').removeClass('is-vcard-reduced');
+        else $('.vcard').addClass('is-vcard-reduced');
+    }
+    
+    function init(){
+        showContactMe();
+        setupHeader();
+        $(window).scroll(setupHeader);
+    }
+    init();
 
 });
