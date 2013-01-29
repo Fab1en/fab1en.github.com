5b7503321308d26f95f0c09a81361cd3a984cc71
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-01-29 23:17:51 +0100

    [dev] Ajouter le carousel de projets

--- js/main.js
+++ js/main.js
@@ -14,10 +14,52 @@ jQuery(function($){
         else $('.vcard').addClass('is-vcard-reduced');
     }
     
+    // Carrousel classique
+    function simpleCarrousel( $carrousel, viewItems, scrollItems ) {
+    
+        var $stage = $carrousel.find('.csel-stage');
+        var $elems = $carrousel.find('.csel-item');
+        var transition = false;
+        var elemsNb     = $elems.length;
+        var elemsWidth  = 0;
+        var index      = 0;
+        var direction  = 'next';
+        
+        if( elemsNb > viewItems ) {
+            
+            $elems.each(function() {
+                elemsWidth += $(this).outerWidth();
+            });
+            
+            $stage.width( elemsWidth );
+            $carrousel
+                .prepend('<div class="navigation"><a href="#" class="prev icon-arrow-left" title="Afficher l\'élément précédent"></a></div>')
+                .append('<div class="navigation"><a href="#" class="next icon-arrow-right" title="Afficher l\'élément suivant"></a></div>');
+                
+            $carrousel.find('.navigation').on('click', 'a', function(e) {
+                e.preventDefault();
+                if( transition == false ) {
+                    transition = true;
+                    if( $(this).hasClass('prev') ) {
+                        index <= 0 ? index = elemsNb-viewItems : index-=scrollItems;
+                        if(index < 0) index = 0;
+                    } else if( $(this).hasClass('next') ) {
+                        index >= elemsNb-viewItems ? index = 0 : index+=scrollItems;
+                        if(index > elemsNb-viewItems) index = elemsNb-viewItems;
+                    }
+                    $stage.animate({ left: - ( elemsWidth / elemsNb ) * index }, 1000, function() { transition = false; });
+                }
+            });
+
+        }
+
+    }
+    
     function init(){
         showContactMe();
         setupHeader();
         $(window).scroll(setupHeader);
+        simpleCarrousel($('.carousel'), 1, 1);
     }
     init();
 
